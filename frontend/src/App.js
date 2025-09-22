28708f311dcd760b61428a5645ef5a81b411b5d1
jonathan-grumke <grumkejonathan@yahoo.de>
2025-09-22 15:42:34 +0200

    rezepte component initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Rezepte from "./components/Rezepte/Rezepte"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/rezepte" element={<Rezepte />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/Rezepte/Rezepte.jsx
@@ -0,0 +1,30 @@
+// import { useState, useEffect } from 'react';
+import "./Rezepte.css";
+import "../assets/styles.css";
+
+export default function Rezepte() {
+
+    // let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
+
+    return (
+        <>
+            <title>Rezepte</title>
+            <div>
+                <h1>Rezepte</h1>
+                <h2>Hauptgerichte</h2>
+                <ul>
+
+                </ul>
+                <h2>Desserts</h2>
+                <ul>
+
+                </ul>
+                <h2>Snacks</h2>
+                <ul>
+
+                </ul>
+            </div>
+        </>
+    )
+
+}
