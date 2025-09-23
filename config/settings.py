e5580968cdbd570c3eae03e4eae2dea2d3c7c86e
jonathan-grumke <grumkejonathan@yahoo.de>
2025-09-23 10:22:31 +0200

    create recipes model

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/public/uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    
+    CATEGORY_CHOICES = [
+        ('main', 'Hauptgericht'),
+        ('salad', 'Salat'),
+        ('dessert', 'Dessert'),
+        ('snack', 'Snack'),
+        ('drink', 'Getränk'),
+        ('soup', 'Suppe'),
+    ]
+    
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='main')
+    ingredients = models.JSONField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
