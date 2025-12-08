4a79bcbb7668d738974305c453e105d8c2baf2b2
jonathan-grumke <grumkejonathan@yahoo.de>
2025-12-08 13:12:29 +0100

    add user role to admin panel form

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Recipe, User
 from django.contrib.auth.admin import UserAdmin
+from .models import Recipe, User
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    
+    list_display = UserAdmin.list_display + ('role',)
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Recipe)
